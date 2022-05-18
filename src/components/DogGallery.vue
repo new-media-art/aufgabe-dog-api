ce1a32647a3e64d0a4d077917eb2e8a24bb27f9e
Ulrico Carmona <ulricocarmona@gmail.com>
2022-05-18 16:09:05 +0200

    x

--- src/components/DogGallery.vue
+++ src/components/DogGallery.vue
@@ -2,30 +2,27 @@
   <v-container>
     <h1>Random dogs</h1>
     <v-row>
-      <v-col 
-      v-for="(dog, index) in numDogs" 
-      :key="index" 
-      cols="3">
-      <v-card>
-           <v-img
-              :src="dog"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="300px"
-            >
-             <v-card-title class="text-uppercase">{{ $store.state.dogBreed[index] }} </v-card-title>
-             
-            </v-img>
+      <v-col v-for="(dog, index) in numDogs" :key="index" cols="3">
+        <v-card>
+          <v-img
+            :src="dog"
+            class="white--text align-end"
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            height="300px"
+          >
+            <v-card-title class="text-uppercase"
+              >{{ $store.state.dogBreed[index] }}
+            </v-card-title>
+          </v-img>
 
-            <v-card-actions>
-              <v-spacer></v-spacer>
+          <v-card-actions>
+            <v-spacer></v-spacer>
 
-              <v-btn icon>
-                <v-icon @click="addToFavorites(dog)">mdi-heart</v-icon>
-              </v-btn>
-
-            </v-card-actions>
-          </v-card>
+            <v-btn icon>
+              <v-icon @click="addToFavorites(dog)">mdi-heart</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -36,35 +33,32 @@ export default {
   name: "DogGallery",
 
   data: () => ({
-      numDog: 1
+    numDog: 1,
   }),
   computed: {
     numDogs() {
-      return this.numberOfDogs()
-    }
+      return this.numberOfDogs();
+    },
   },
-   
+
   methods: {
     fetchDogs() {
-      this.$store.dispatch('getRandomDogs')
+      this.$store.dispatch("getRandomDogs");
     },
-    addToFavorites (dog) {
-      this.$store.dispatch('addToFavorites',  dog )
+    addToFavorites(dog) {
+      this.$store.dispatch("addToFavorites", dog);
     },
     numberOfDogs() {
       if (this.numDog === 1) {
-        return   this.$store.state.randomDogs
+        return this.$store.state.randomDogs;
       }
       if (this.numDog === 2) {
-        return   this.$store.state.randomDogs.slice(0, 3)
+        return this.$store.state.randomDogs.slice(0, 3);
       }
-      
-      
-    }
+    },
   },
-  mounted () {
-    this.fetchDogs()
-  }
-  
+  mounted() {
+    this.fetchDogs();
+  },
 };
 </script>
